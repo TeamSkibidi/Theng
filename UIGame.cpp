f8a2ebbf36eab5c6459c8af530285da92e084aa3
TeamSkibidi <skibiditolet553@gmail.com>
2025-03-08 21:13:51 +0700

    Update UIGame.cpp

--- UIGame.cpp
+++ UIGame.cpp
@@ -1,24 +1,120 @@
-#include <SFML/Graphics.hpp>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <ctime>
-#include <sstream>
-#include "UIGame.h"
-#include "DashBoard.h"
+	#include <SFML/Graphics.hpp>
+	#include <iostream>
+	#include <string>
+	#include <vector>
+	#include <ctime>
+	#include <sstream>
+	#include "UIGame.h"
+	#include "DashBoard.h"
+	#include "Grid.h"
 
+
+void UIGame::checkFPS()
+{
+
+	this->window->setFramerateLimit(60);
+	double ending = clock.getElapsedTime().asSeconds();
+	double timeFPS = ending - starting;
+	string FPS;
+
+	if (timeFPS > 0.0001) {
+		FPS = to_string(int(1.0 / timeFPS));
+	}
+	this->window->setTitle("2048 FPS: " + FPS);
+	this->starting = ending;
+}
 
 void UIGame::initWindow() {
-	this->videomode = 
-	this->window = new sf::RenderWindow(sf::VideoMode(600, 800));
+
+		this->videomode = sf::VideoMode(600,800);
+		this->window = new sf::RenderWindow(this->videomode, "2048");
 	
-}
-UIGame::UIGame() {
-	this->initDashBoard();
+	}
+	void UIGame::initDashBoard()
+	{
+		this->dashboard = new DashBoard(this->window);
+	}
 
-}
-void UIGame::initDashBoard()
-{
-	this->dashboard = new DashBoard(this->window);
+	void UIGame::initGrid()
+	{
+		this->grid = new GridGame(this->window);
+	}
 
-}
+	UIGame::UIGame() 
+	{
+		this->initWindow();
+		this->initDashBoard();
+		this->initGrid();
+
+
+	}
+
+
+	UIGame::~UIGame()
+	{
+		delete this->dashboard;
+		this->dashboard = nullptr;
+
+
+		delete this->window;
+		this->window = nullptr;
+
+	}
+
+
+	const bool UIGame::running() const
+	{
+		return this->window->isOpen();
+	}
+
+
+	void UIGame::pollevent()
+	{
+		if (!this->window) return;
+
+
+		this->view = sf::View(sf::FloatRect(0, 0, 600, 800));
+
+		while (this->window->pollEvent(event)) {
+			if (this->event.type == sf::Event::Closed) {
+				this->window->close();
+			}
+			else if (this->event.type == sf::Event::Resized) {
+				float Update_width = static_cast<float>(this->event.size.width);
+				float Update_height = static_cast<float>(this->event.size.height);
+
+				float Update_board = Update_width / Update_height;
+				float board = 600.0f / 800.0f;
+
+				if (Update_board > board) {
+					this->view.setSize(static_cast <float>(800 * Update_board), static_cast <float>(800));
+				}
+				else {
+					this->view.setSize(static_cast <float>(600), static_cast <float>(600 / Update_board));
+				}
+				this->window->setView(view);
+			}
+		}
+	}
+
+	void UIGame::update()
+	{
+		this->pollevent();
+	}
+
+	void UIGame::render()
+	{
+		checkFPS();
+		if (!this->window)
+			return;
+
+		this->window->clear(sf::Color(245, 243, 227));
+		this->dashboard->render();
+		this->grid->RenderGridGame();
+		this->window->display();
+	}
+
+
+	
+
+
