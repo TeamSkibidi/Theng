8974b8825a278637c7b64b30b16b54beec18a976
TeamSkibidi <skibiditolet553@gmail.com>
2025-03-08 21:11:57 +0700

    Update DashBoard.h

--- DashBoard.h
+++ DashBoard.h
@@ -1,9 +1,11 @@
 #pragma once
+
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
 #include <iostream>
-#include <vector>
-#include <ctime>
-#include <sstream>
 #include <string>
 
 using namespace std;
@@ -11,28 +13,24 @@ using namespace std;
 class DashBoard {
 private:
 	sf::RenderWindow* window;
-	sf::Text newText;
-	string fileImage;
 	sf::RectangleShape rec[6];
 	sf::Text newText[3];
-	sf::Font font;
 	sf::Sprite newSprite[4];
+	sf::Font font;
+	sf::Texture textures[4];
 
 
-
-	void initFont(sf::Font &font);
 	void initRectangle(int index, float x, float y, sf::Color color, float sizeWidth, float sizeHight);
 	void initText(int index, sf::Font font, string text, int size, float x, float y, sf::Color color);
-	void initPng(int index, const string& path, float x, float y, sf::Color color, float sizeX, float sizeY);
+	void initPng(int index, const string& path, float x, float y, float sizeX, float sizeY, sf::Color color);
 	void initAllRec();
 	void initAllimage();
 	void initAllText();
 public:
 	DashBoard(sf::RenderWindow* window);
 
+	void initFont(sf::Font& font);
 	void presssEvent(sf::Event event);
 	void render();
-	
-
 
 };
