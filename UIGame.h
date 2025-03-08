ed88280416392f47a1e14650016c055307bfbb7f
TeamSkibidi <skibiditolet553@gmail.com>
2025-03-08 21:14:04 +0700

    Update UIGame.h

--- UIGame.h
+++ UIGame.h
@@ -1,24 +1,38 @@
 #pragma once
-#include <SFML/Graphics.hpp>c
-#include "Dashboard.h"
+
+
+#include <SFML/Graphics.hpp>
+
+
+class DashBoard;
+class GridGame;
 
 
 class UIGame {
 private:
 	sf::RenderWindow* window;
 	DashBoard* dashboard;
-	sf::VideoMode* videomode;
+	sf::VideoMode videomode;
+	sf::Event event;
+	sf::View view;
+	GridGame* grid;
+	sf::Clock clock;
+	double starting = 0.0001;
 
-
+	void checkFPS();
 	void initWindow();
 	void initDashBoard();
+	void initGrid();
 
 public:
 	UIGame();
 	~UIGame();
 
 
-	void pollEvent();
+	const bool running() const;
+
+
+	void pollevent();
 	void update();
 	void render();
 };
